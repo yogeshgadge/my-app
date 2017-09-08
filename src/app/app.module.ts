0244b1cc53e3d237ea097aeb10d09465a7c1ca2d
Yogesh <themodernizers@gmail.com>
2017-09-08 01:03:39 -0400

    Step 4: Add imports to app.module. (Note no Material modules being imported explicitly.)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {
+  CBPRootModule,
+  CBPHeaderModule,
+  CBPAppHeaderModule,         // this is optional
+  CBP_USER_SERVICE,           // must be provided to fetch CBP user logged in to display user name on CBP Header
+  CBP_APPLICATIONS_SERVICE    // must be provided to get CBP applications (recent) for Applications menu on the CBP Header
+} from 'ngx-cbp-theme' ;
+import {MyUserService} from './my-user.service';
+import {MyApplicationsService} from './my-applications.service';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CBPRootModule,
+    CBPAppHeaderModule
   ],
-  providers: [],
+  providers: [
+    { provide: CBP_USER_SERVICE,          useClass: MyUserService },
+    { provide: CBP_APPLICATIONS_SERVICE,  useClass: MyApplicationsService }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
